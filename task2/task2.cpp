b537df7f674ad74d6f332bfc15cbf774c12eb417
ziabric <kurbet78@list.ru>
2023-12-13 13:39:59 +0300

    minor

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,1 +1,54 @@
- 
+#pragma once
+#include <sstream>
+#include <vector>
+#include <string>
+#include <iostream>
+
+std::vector<std::vector<int>> parseGraph(const std::string& csv, int& maxNode) {
+    std::vector<std::vector<int>> graph;
+    std::stringstream ss(csv);
+    std::string line;
+    maxNode = 0;
+
+    while (std::getline(ss, line)) {
+        std::stringstream lineStream(line);
+        int parent, child;
+        lineStream >> parent;
+        lineStream.ignore(1);
+        lineStream >> child;
+
+        maxNode = std::max(maxNode, std::max(parent, child));
+
+        while (graph.size() <= maxNode) {
+            graph.push_back(std::vector<int>());
+        }
+
+        graph[parent].push_back(child);
+    }
+    return graph;
+}
+
+std::vector<std::vector<int>> computeParents(const std::vector<std::vector<int>>& graph, int maxNode) {
+    std::vector<std::vector<int>> parents(maxNode + 1);
+
+    for (int i = 0; i < graph.size(); ++i) {
+        for (int child : graph[i]) {
+            parents[child].push_back(i);
+        }
+    }
+
+    return parents;
+}
+
+std::string task(const std::string &csv)
+{
+    int maxNode;
+    auto graph = parseGraph(csv, maxNode);
+    auto parents = computeParents(graph, maxNode);
+
+    std::vec
+    for (int i = 0; i < maxNode; i += 1)
+    {
+
+    }
+}
---
+++ tests/test2.cpp
@@ -0,0 +1,26 @@
+#include <task2/task2.cpp>
+int main() {
+    std::string csv = "0,1\n0,2\n1,3\n2,4";
+    int maxNode;
+    auto graph = parseGraph(csv, maxNode);
+    auto parents = computeParents(graph, maxNode);
+
+    // Вывод результатов
+    for (int i = 0; i <= maxNode; ++i)
+    {
+        std::cout << "Node: " << i << "\n";
+        std::cout << "Children: ";
+        for (int child : graph[i])
+        {
+            std::cout << child << " ";
+        }
+        std::cout << "\nParents: ";
+        for (int parent : parents[i])
+        {
+            std::cout << parent << " ";
+        }
+        std::cout << "\n\n";
+    }
+
+    return 0;
+}
