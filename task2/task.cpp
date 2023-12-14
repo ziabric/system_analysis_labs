24dd0a6d30553a922592f7d5e18d5c467c2dce08
ziabric <kurbet78@list.ru>
2023-12-14 03:25:55 +0300

    complete task2

--- task2/task.cpp
+++ task2/task.cpp
@@ -1,54 +1,110 @@
 #pragma once
-#include <sstream>
-#include <vector>
-#include <string>
+
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
 
-std::vector<std::vector<int>> parseGraph(const std::string& csv, int& maxNode) {
-    std::vector<std::vector<int>> graph;
-    std::stringstream ss(csv);
-    std::string line;
-    maxNode = 0;
+std::unordered_map<int, std::vector<int>> children, parents;
+std::vector<int> knots = {};
+int allParents = 0;
+int allChildren = 0;
+int parentChildren = 0;
 
-    while (std::getline(ss, line)) {
-        std::stringstream lineStream(line);
-        int parent, child;
-        lineStream >> parent;
-        lineStream.ignore(1);
-        lineStream >> child;
 
-        maxNode = std::max(maxNode, std::max(parent, child));
-
-        while (graph.size() <= maxNode) {
-            graph.push_back(std::vector<int>());
-        }
-
-        graph[parent].push_back(child);
-    }
-    return graph;
-}
-
-std::vector<std::vector<int>> computeParents(const std::vector<std::vector<int>>& graph, int maxNode) {
-    std::vector<std::vector<int>> parents(maxNode + 1);
-
-    for (int i = 0; i < graph.size(); ++i) {
-        for (int child : graph[i]) {
-            parents[child].push_back(i);
-        }
-    }
-
-    return parents;
-}
-
-std::string task(const std::string &csv)
+void findParent(int currentKnot)
 {
-    int maxNode;
-    auto graph = parseGraph(csv, maxNode);
-    auto parents = computeParents(graph, maxNode);
+    allParents += parents[currentKnot].size();
+    for (auto item : parents[currentKnot])
+    {
+        findParent(item);
+    }
+}
 
-    // std::vec
-    // for (int i = 0; i < maxNode; i += 1)
-    // {
+void findChildren(int currentKnot)
+{
+    allChildren += children[currentKnot].size();
+    for (auto item : children[currentKnot])
+    {
+        findChildren(item);
+    }
+}
 
-    // }
+std::string processGraphCSV(const std::string& csv) {
+    children.clear(); 
+    parents.clear();
+    knots.clear();
+
+    std::istringstream ss(csv);
+    std::string token;
+    while (std::getline(ss, token)) {
+        std::stringstream lineSS(token);
+        std::string parent, child;
+        getline(lineSS, parent, ',');
+        getline(lineSS, child, '\n');
+        int parentID = std::stoi(parent);
+        int childID = std::stoi(child);
+
+        children[parentID].push_back(childID);
+        parents[childID].push_back(parentID);
+
+        if ( std::find(knots.begin(), knots.end(), parentID) == knots.end() )
+        {
+            knots.push_back(parentID);
+        }
+        if ( std::find(knots.begin(), knots.end(), childID) == knots.end() )
+        {
+            knots.push_back(childID);
+        }
+    }
+    std::sort(knots.begin(), knots.end());
+
+    std::vector<std::vector<int>> result;
+
+    for (const auto& node : knots) 
+    {
+        int nodeID = node;
+        std::vector<int> nodeData;
+
+        nodeData.push_back(children[nodeID].size()); 
+        nodeData.push_back(parents[nodeID].size());  
+
+        allChildren = 0;
+        allParents = 0;
+        parentChildren = 0;
+
+        for (auto item : children[nodeID])
+        {
+            findChildren(item);
+        }
+        nodeData.push_back(allChildren);
+
+        for (auto item : parents[nodeID])
+        {
+            findParent(item);
+        }
+        nodeData.push_back(allParents);
+
+        for (auto parent : parents[nodeID])
+        {
+            parentChildren += children[parent].size();
+        }
+        nodeData.push_back(parentChildren == 0 ? 0 : parentChildren - 1);
+
+        result.push_back(nodeData);
+    }
+
+    std::string output = "";
+
+    for (const auto& node : result) 
+    {
+        for (int val : node) {
+            output += std::to_string(val);
+            output += ',';
+        }
+        output[output.size() - 1] = '\n';
+    }
+
+    return output; 
 }
--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,26 +1,13 @@
 #include <task2/task.cpp>
-int main() {
-    std::string csv = "0,1\n0,2\n1,3\n2,4";
-    int maxNode;
-    auto graph = parseGraph(csv, maxNode);
-    auto parents = computeParents(graph, maxNode);
 
-    // Вывод результатов
-    for (int i = 0; i <= maxNode; ++i)
-    {
-        std::cout << "Node: " << i << "\n";
-        std::cout << "Children: ";
-        for (int child : graph[i])
-        {
-            std::cout << child << " ";
-        }
-        std::cout << "\nParents: ";
-        for (int parent : parents[i])
-        {
-            std::cout << parent << " ";
-        }
-        std::cout << "\n\n";
-    }
+// 1,2\n1,3\n3,4\n3,5
+
+int main() 
+{
+    std::string csv = "1,2\n1,3\n3,4\n3,5";
+    auto result = processGraphCSV(csv);
+
+    std::cout<<result<<std::endl;
 
     return 0;
 }
